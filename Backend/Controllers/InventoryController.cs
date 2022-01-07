7cf77b8e3fe0c47ad130df20b5f90b7e32a7ac93
AdnanJoraid <adnanjoraidca@gmail.com>
2022-01-07 13:58:49 -0500

    added GET for items by ID, all Items and Inventories

--- Backend/Controllers/InventoryController.cs
+++ Backend/Controllers/InventoryController.cs
@@ -16,14 +16,16 @@ namespace Backend.Controllers
     /*
 
     endpoints 
-    POST Inventory - Creates an Inventory
+    POST Inventory - Creates an Inventory DONE
     POST Items(JSON) - adds multiple items 
-    POST Item(Item item) - adds one item 
+    POST Item(Item item) - adds one item DONE 
     POST Item To inventory - Add Item to a specified Inventory
-    GET Item by ID 
-    GET all items 
+    GET Item by ID DONE
+    GET all items DONE
     Edit Item by Id
-    GET List of items 
+    GET List of items DONE
+    DELETE Inventory by id 
+    DELETE Item By id 
     */
 
     public class InventoryController : ControllerBase
@@ -83,7 +85,7 @@ namespace Backend.Controllers
 
                 item.DateOfCreation = DateTime.Now;
                 item.IsAvailable = item.QuantityStock > 0 ? true : false;
-                var inventory = await _context.Inventory.Include(x => x.Items).FirstOrDefaultAsync(x => x.Name.Equals("LogisticsDefaultInventory"));
+                var inventory = await _context.Inventory.Include(x => x.Items).FirstOrDefaultAsync(x => x.Name.Equals("LogisticsDefaultInventory")); //default inventory
                 inventory.Items.Add(item);
                 await _context.SaveChangesAsync();
 
@@ -96,13 +98,60 @@ namespace Backend.Controllers
             }
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetItems()
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItemById(string id)
         {
+            try
+            {
+                Item item = await _context.Items.FirstOrDefaultAsync(x => x.Id.Equals(new Guid(id)));
 
-            return Ok(_context.Inventory.Include(x => x.Items).ToList());
+                if (item == null)
+                {
+                    return BadRequest($"Item with the id {id} does not exist.");
+                }
+
+                return Ok(item);
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Item with the id {id} does not exist. For a detailed error, read below.\n\n\n{e.ToString()}");
+            }
         }
 
+        [HttpGet("All")]
+        public async Task<IActionResult> GetAllDefaultInventoryItem()
+        {
+            //returns all items in default inventory
+            try
+            {
+                var inventory = await _context.Inventory.Include(x => x.Items).FirstOrDefaultAsync(x => x.Name.Equals("LogisticsDefaultInventory")); //default inventory
+                return Ok(inventory);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
+
+
+        [HttpGet("{name}/All")]
+        public async Task<IActionResult> GetAllInventoryItemByName(string name)
+        {
+            //returns all items in specified inventory
+            try
+            {
+                var inventory = await _context.Inventory.Include(x => x.Items).FirstOrDefaultAsync(x => x.Name.Equals(name));
+                if (inventory == null)
+                    return BadRequest($"Inventory with name: {name} does not exist");
+                return Ok(inventory);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.ToString());
+            }
+        }
 
     }
 }
