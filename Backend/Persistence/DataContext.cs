e4967d4fd8f11814408310bb94c8a8ea0bf4cec2
AdnanJoraid <adnanjoraidca@gmail.com>
2022-01-06 23:05:21 -0500

    added dbset (tables) in dbcontext class

--- Backend/Persistence/DataContext.cs
+++ Backend/Persistence/DataContext.cs
@@ -1,10 +1,14 @@
 using Microsoft.EntityFrameworkCore;
-
+using Backend.Models;
 
 namespace Backend.Persistence
 {
     public class DataContext : DbContext
     {
+
+        public DbSet<Item> Items {get; set;}
+
+        public DbSet<Inventory> Inventory { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Filename=Database.db");
