4c1a2757585eb814e557c963af989decdcadbf51
AdnanJoraid <adnanjoraidca@gmail.com>
2022-01-09 11:38:16 -0500

    Added routes fort create and view item views

--- Frontend/logisticsfrontend/src/App.js
+++ Frontend/logisticsfrontend/src/App.js
@@ -7,12 +7,16 @@ import {
   BrowserRouter,
 } from "react-router-dom";
 import HomeView from "./Views/HomeView";
+import CreateView from "./Views/CreateView";
+import ViewItem from "./Views/ViewItems";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<HomeView />} />
+        <Route path="/Create" element={<CreateView />} />
+        <Route path="/View" element={<ViewItem />} />
       </Routes>
     </Router>
   );
