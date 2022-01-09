03e3a76028bc0ffd35248352bb485cd7629f3361
AdnanJoraid <adnanjoraidca@gmail.com>
2022-01-09 11:37:47 -0500

    implemented navigation from homeview to other views

--- Frontend/logisticsfrontend/src/Views/HomeView.js
+++ Frontend/logisticsfrontend/src/Views/HomeView.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "../Styles/HomeView.css";
-
+import { useNavigate } from "react-router-dom";
 function HomeView() {
+  const navigate = useNavigate();
   return (
     <div className="center" style={{ margin: 55 }}>
       <h1>
@@ -9,12 +10,17 @@ function HomeView() {
         Joraid
       </h1>
       <div className="center" style={{ margin: 300 }}>
-        <button style={{ width: 350, height: 50 }} className="buttonStyle">
+        <button
+          style={{ width: 350, height: 50 }}
+          className="buttonStyle"
+          onClick={() => navigate("/Create")}
+        >
           Create items, warehouses, and transactions
         </button>
         <button
           style={{ width: 350, height: 50 }}
           className="buttonStyleSecondary"
+          onClick={() => navigate("/View")}
         >
           View items, warehouses, and transactions
         </button>
