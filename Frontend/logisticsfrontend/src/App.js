a490090c7d1ffff319da98da1272ac75a2a1192e
AdnanJoraid <adnanjoraidca@gmail.com>
2022-01-09 10:25:31 -0500

    added React router

--- Frontend/logisticsfrontend/src/App.js
+++ Frontend/logisticsfrontend/src/App.js
@@ -1,6 +1,21 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
+import HomeView from "./Views/HomeView";
 
 function App() {
-  return <div>Hello from React</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomeView />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ Frontend/logisticsfrontend/src/Views/HomeView.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function HomeView() {
+  return <h1>Home Screen</h1>;
+}
+
+export default HomeView;
