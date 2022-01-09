895d7bc6644c62f068cde1b16b841bc9d6162c0a
AdnanJoraid <adnanjoraidca@gmail.com>
2022-01-09 12:24:28 -0500

    Added routes for view components

--- Frontend/logisticsfrontend/src/App.js
+++ Frontend/logisticsfrontend/src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  BrowserRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomeView from "./Views/HomeView";
 import CreateView from "./Views/CreateView";
 import ViewItem from "./Views/ViewItems";
+import WarehousesView from "./Views/WarehousesView";
+import TransactionsView from "./Views/TransactionsView";
+import InventoryItemsView from "./Views/InventoryItemsView";
 
 function App() {
   return (
@@ -17,6 +14,9 @@ function App() {
         <Route exact path="/" element={<HomeView />} />
         <Route path="/Create" element={<CreateView />} />
         <Route path="/View" element={<ViewItem />} />
+        <Route path="/View/InventoryItems" element={<InventoryItemsView />} />
+        <Route path="/View/Warehouses" element={<WarehousesView />} />
+        <Route path="/View/Transactions" element={<TransactionsView />} />
       </Routes>
     </Router>
   );
