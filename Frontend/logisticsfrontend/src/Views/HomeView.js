7d6c462221fefb1a32bcd5e107befc419dcd6ea2
AdnanJoraid <adnanjoraidca@gmail.com>
2022-01-09 10:56:45 -0500

    finished basic view and design for home view

--- Frontend/logisticsfrontend/src/Views/HomeView.js
+++ Frontend/logisticsfrontend/src/Views/HomeView.js
@@ -1,8 +1,27 @@
 import React from "react";
-
+import { Stack, Button } from "react-bootstrap/";
+import "../Styles/HomeView.css";
 
 function HomeView() {
-  return <h1>Home Screen</h1>;
+  return (
+    <div className="center" style={{ margin: 55 }}>
+      <h1>
+        Welcome To Logistics - Shopify summer 2022 backend challenge - By Adnan
+        Joraid
+      </h1>
+      <div className="center" style={{ margin: 300 }}>
+        <button style={{ width: 350, height: 50 }} className="buttonStyle">
+          Create items, warehouses, and transactions
+        </button>
+        <button
+          style={{ width: 350, height: 50 }}
+          className="buttonStyleSecondary"
+        >
+          View items, warehouses, and transactions
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default HomeView;
