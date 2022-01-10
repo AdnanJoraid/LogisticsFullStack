607e864867417c5274dc69fd11e6abefa6f067c0
AdnanJoraid <adnanjoraidca@gmail.com>
2022-01-10 15:47:39 -0500

    added create view to link creations of items, warehouses and transactions

--- Frontend/logisticsfrontend/src/Views/CreateView.js
+++ Frontend/logisticsfrontend/src/Views/CreateView.js
@@ -1,10 +1,40 @@
 import React from "react";
+import "../Styles/HomeView.css";
+import { useNavigate, Link } from "react-router-dom";
 
 const CreateView = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Create Views</h1>
-      <p>test</p>
+    <div className="center" style={{ margin: 55 }}>
+      <h1>
+        <Link to={"/"}>Home</Link>
+      </h1>
+      <div style={{ margin: 280 }}>
+        <button
+          style={{ width: 350, height: 50, margin: 10 }}
+          className="buttonStyle"
+          onClick={() => navigate("/View/InventoryItems")}
+        >
+          Create an Inventory Item
+        </button>
+        <div></div>
+        <button
+          style={{ width: 350, height: 50, margin: 10 }}
+          className="buttonStyle"
+          onClick={() => navigate("/View/Warehouses")}
+        >
+          Create a Warehouse
+        </button>
+        <div></div>
+        <button
+          style={{ width: 350, height: 50, margin: 10 }}
+          className="buttonStyle"
+          onClick={() => navigate("/Create/Transaction")}
+        >
+          Create a Transaction
+        </button>
+      </div>
     </div>
   );
 };
